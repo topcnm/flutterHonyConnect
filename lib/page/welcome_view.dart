4bfc00fa781aa4457f5a761df1d3cc2cb8557870
zhouwangsheng <zhouwangsheng@gome.com.cn>
2018-11-20 09:37:45 +0800

    hhggiiddasd

--- lib/page/welcome_view.dart
+++ lib/page/welcome_view.dart
@@ -13,13 +13,33 @@ class Welcome extends StatefulWidget {
   _WelcomeState createState() => new _WelcomeState();
 }
 
-class _WelcomeState extends State<Welcome> {
+class _WelcomeState extends State<Welcome> with TickerProviderStateMixin{
+  AnimationController controller;
+  Animation<double> animation;
+  var animationStateListener;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-//    return null;
+    controller = new AnimationController(vsync: this, duration: new Duration(seconds: 2));
+    animation = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    animationStateListener = (status){
+      if (status == AnimationStatus.completed) {
+        try{
+          Navigator.of(context).pushAndRemoveUntil(
+              new MaterialPageRoute(builder: (BuildContext context) => new LoginPage()),
+          (Route route) => route == null);
+        } catch(e) {
+          print('Leading page error');
+        }
+      }
+    };
+
+    animation.addStatusListener(animationStateListener);
+    controller.forward();
+
+    return null;
     new Timer(new Duration(milliseconds: 2000), () {
       try{
         Navigator.of(context).pushAndRemoveUntil(
@@ -32,14 +52,36 @@ class _WelcomeState extends State<Welcome> {
   }
 
   @override
+    void dispose() {
+      // TODO: implement dispose
+      animation.removeStatusListener(animationStateListener);
+      controller.dispose();
+      super.dispose();
+    }
+
+  @override
   Widget build(BuildContext context) {
     ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
 
     return new Material(
-      color: pressColor,
       child: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
+          // new ClipRect(
+          //   child: new Align(
+          //     alignment: Alignment.center,
+          //     child: new Image.asset('lib/images/welcome.jpg'),
+          //   )
+          // ),
+          new FadeTransition(
+            opacity: animation,
+            child: new Image(
+              image: new AssetImage('lib/images/welcome.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          
+       
           new Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
